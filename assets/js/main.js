9d5bdcd6b8d0ad579bca895997716c633c410936
kikukeii <miftakhuddinfalaki@gmail.com>
2023-03-22 14:13:08 +0700

    add online page on dashboad, fix config firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -329,3 +329,5 @@ function cv404() {
   const showPesan2 = new bootstrap.Toast(getPesan2)
   showPesan2.show()
 }
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
